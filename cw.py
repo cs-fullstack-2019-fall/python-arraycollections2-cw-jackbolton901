a576d5e4a6a3397aa7f7c7cd5c98706fee2da3be
jackbolton901 <bltnjck@gmail.com>
2019-08-27 16:51:57 -0500

    08-27-2019_classwork

---
+++ cw.py
@@ -0,0 +1,40 @@
+# problem 1
+
+person_array = ["Kenn", "Kevin", "Erin", "Autumn"]
+
+def stupid_array_tricks(person_array):
+        # a)
+    print(person_array[1:3])
+        # b)
+    print(person_array[2:5])
+        # c)
+    person_array.insert(2, "Chuck")
+    print(person_array)
+        # d)
+    person_array.pop(1)
+    print(person_array)
+stupid_array_tricks(person_array)
+
+# Problem 2
+userIn = ""
+while userIn != q:
+    userIn = input("Enter something or press q to quit")
+
+# Problem 3
+
+dataArray = ['GOOD_DATA', 'DECENT_DATA', 'BAD_DATA', 'DECENT_DATA', 'GOOD_DATA',
+             'BAD_DATA', 'GOOD_DATA', 'DECENT_DATA', 'BAD_DATA', 'GOOD_DATA']
+
+print(len(dataArray))
+dataArray.remove('BAD_DATA')
+print(len(dataArray))
+
+
+score_list = [21, 14, 6, 8, 28, 42, 21]
+sum = (sum(score_list))
+max = max(score_list)
+min = min(score_list)
+
+print(f'The sum of all scores is {sum}.')
+print(f'The maximum number is {max}')
+print(f'The minimum number is {min}')
